5c833b3c2ee3cdec0a810acd51a0c4068422f65b
sawsan altammar <sozana@Johns-MacBook-Air.local>
2013-02-23 22:33:05 -0500

    Initial Commit

---
+++ WhatsInMyPocket/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WhatsInMyPocket
+//
+//  Created by sawsan altammar on 2/23/13.
+//  Copyright (c) 2013 sawsan altammar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ WhatsInMyPocket/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  WhatsInMyPocket
+//
+//  Created by sawsan altammar on 2/23/13.
+//  Copyright (c) 2013 sawsan altammar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
