24b60026200524973ad51614f8caccc822dc525f
sawsan altammar <sozana@Johns-MacBook-Air.local>
2013-03-19 21:45:12 -0700

    Changed a comment

--- WhatsInMyPocket/CalculateTableViewController.m
+++ WhatsInMyPocket/CalculateTableViewController.m
@@ -44,7 +44,7 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // A change :)
 }
 
 #pragma mark - Table view data source
