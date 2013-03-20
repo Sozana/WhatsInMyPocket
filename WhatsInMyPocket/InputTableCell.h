736143e3937208e8430cb718c0d40f42d09479f2
sawsan altammar <sozana@Johns-MacBook-Air.local>
2013-03-19 21:41:20 -0700

    First commit

---
+++ WhatsInMyPocket/InputTableCell.h
@@ -0,0 +1,15 @@
+//
+//  InputTableCell.h
+//  WhatsInMyPocket
+//
+//  Created by sawsan altammar on 2/23/13.
+//  Copyright (c) 2013 sawsan altammar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InputTableCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UITextField *inputField;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
+@end
---
+++ WhatsInMyPocket/InputTableCell.m
@@ -0,0 +1,29 @@
+//
+//  InputTableCell.m
+//  WhatsInMyPocket
+//
+//  Created by sawsan altammar on 2/23/13.
+//  Copyright (c) 2013 sawsan altammar. All rights reserved.
+//
+
+#import "InputTableCell.h"
+
+@implementation InputTableCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
